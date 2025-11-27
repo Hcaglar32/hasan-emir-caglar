8635f62339f6c714c28d7e371d3155b2f166b9e2
Hcaglar32 <83988785+Hcaglar32@users.noreply.github.com>
2025-11-27 14:23:17 +0300

    Revert "Bazı hatalar giderildi."
    This reverts commit 2556cd3858b7c4a09355caa73fbc67f7e3d9e6e2.

--- components/Navbar/MobileMenu.tsx
+++ components/Navbar/MobileMenu.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import NavItem from "./NavItem";
 
-interface Link {
-  path: string;
-  title: string;
+interface MobileMenuProps {
+  links: [];
 }
-
-interface MobileMenuprops {
-  links: Link[];
-}
-
-const MobileMenu = ({ links }: MobileMenuprops) => {
-  return (
+const MobileMenu = ({links}:MobileMenuProps) => {
+  return(
     <ul className="flex flex-col py-4 items-center z-10 bg-mycolor-400">
-      {links.map((link, index) => (
+      {links.map((link,index)=>(
         <li key={index}>
-          <NavItem href={link.path} title={link.title}></NavItem>
+          <NavItem href={link.path} title={link.title}/>
         </li>
       ))}
     </ul>
